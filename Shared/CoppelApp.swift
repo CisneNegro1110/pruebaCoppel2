e1dfea71c76cabcad7768b12f545892f1c0ad007
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-07-03 11:54:15 -0600

    Initial Commit

---
+++ Shared/CoppelApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoppelApp.swift
+//  Shared
+//
+//  Created by Jesús Francisco Leyva Juárez on 03/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CoppelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
